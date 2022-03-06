d755503bc8aa92471a107e727767d18e5ec47e6f
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-03-06 17:47:42 +0100

    added Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,38 +1,36 @@
 // Header der Seite
+import { HeaderGlobalAction, HeaderGlobalBar, HeaderMenu, HeaderNavigation, HeaderMenuItem, HeaderName, Header } from "carbon-components-react";
 import React, {Fragment, useState} from "react";
-import { Navbar, Container } from "react-bootstrap"
+import { AppSwitcher20 } from '@carbon/icons-react';
 
 // Beinhaltet das Logo und Navigationsbuttons
 
-export function Header(props: {
+export function AppHeader(props: {
     UserName: String
 }) {
 
     return (
         <Fragment>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                <Navbar.Brand href="#home">
+            <Header>
+                <HeaderMenuItem>
                     <img
                         alt=""
-                        src="/logo_offiziell_17_-01.png"
-                        width="30"
+                        src="/logo_offiziell_17_-04.png"
+                        // width="30"
                         height="30"
                         className="d-inline-block align-top"
-                        />
-                    ASV-Aachen Mitglieder
-                </Navbar.Brand>
-
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-
-                <Navbar.Text>
-                    Angemeldet als: {probs.UserName}
-                </Navbar.Text>
-
-                </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                    />
+                </HeaderMenuItem>
+                <HeaderName href="#home" prefix="ASV">
+                    Mitglieder Verwaltung
+                </HeaderName>
+                <HeaderMenuItem >
+                    <p>Angemeldet als: {props.UserName}</p>
+                </HeaderMenuItem>
+                <HeaderNavigation aria-label="IBM [Platform]">
+                    <HeaderMenuItem href="#home">Zurück zur Website</HeaderMenuItem>
+                </HeaderNavigation>
+            </Header>
         </Fragment>
     )
     
