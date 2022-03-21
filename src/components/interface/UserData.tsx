5f59f7f9c181a16090303dda2027a0bcc30a063f
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-03-21 09:32:11 +0100

    added status

--- src/components/interface/UserData.tsx
+++ src/components/interface/UserData.tsx
@@ -1,4 +1,6 @@
 import userEvent from "@testing-library/user-event";
+import { Column, Grid, Row, Tag } from "carbon-components-react";
+import { Fragment } from "react";
 import { Arbeitsstunden, Data, Keycloak, Website } from "./ApiData";
 
 export interface User{
@@ -7,19 +9,34 @@ export interface User{
     username:           string
     first_name:         string
     last_name:          string
+    status:             string
 
     website:            Website
-    arbeitsstuden:      Arbeitsstunden
+    arbeitsstuden:      Arbeitsstunden | undefined
     keycloak:           Keycloak
 }
 
 export interface UserData{
     data:          User[]
+    deaktivated:   User[]
+}
+
+function status(statusID: string): string{
+    switch (statusID) {
+        case "1": return "Anwärter"; 
+        case "2": return "Aktiv"; 
+        case "3": return "Inaktiv"; 
+        case "4": return "Alter Herr"; 
+        case "5": return "Außerordentliches Mitglied"; 
+        case "6": return "Ehrenmitglied"; 
+        default: return "Status not found" 
+    }
 }
 
 export function transformData(json:Data): UserData {
     var erg: UserData = {
-        data: []
+        data: [],
+        deaktivated: []
     }
     json.WEBSITE.forEach((site)=>{
         var erg_key:                Keycloak
@@ -27,23 +44,27 @@ export function transformData(json:Data): UserData {
 
         // TODO: Funktion optimieren
         var erg_key = json.KEYCLOAK.filter((el) => {return el.Email === site.email})[0]
-        var erg_arbeitsstunden = json.ARBEITSSTUNDEN.filter((el) => {return el.email === site.email})[0]
+        if (site.email !== "" && erg_key !== undefined && site !== undefined){
+            var erg_arbeitsstunden = json.ARBEITSSTUNDEN.filter((el) => {return el.email === site.email})[0]
 
-        try {
             var temp: User = {
                 website: site,
                 keycloak: erg_key,
                 arbeitsstuden: erg_arbeitsstunden,
+
                 id: erg_key.id,
+                status: status(site.status), 
                 Email: erg_key.Email,
                 username: erg_key.username,
                 first_name: erg_key.first_name,
                 last_name: erg_key.last_name
             }
-            erg.data.push(temp)
-        } catch (error) {
-            // TODO: Was machen wir wenn nutzer nur in Keycloak oder nur in der AB Datenbank stehen?
-            // console.error(error)   
+
+            if (!site.is_active && !erg_key.enabled ){
+                erg.deaktivated.push(temp)
+            }else{
+                erg.data.push(temp)
+            }
         }
     })
 
